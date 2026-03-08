74c4d2e933f9295c24fe4dbd8adaa3cd0cd7b8bf
Mihir Kacha <kachamihir11@gmail.com>
2026-03-08 22:42:19 +0530

    demo added

---
+++ client_component_demo/src/app/components/Counter.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState } from "react"
+import "./counter.css"
+
+export default function Counter() {
+  const [count, setCount] = useState<number>(0)
+
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Client Component Example</h1>
+
+        <p className="label">Current Count</p>
+
+        <p className="count">{count}</p>
+
+        <button
+          className="btn"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment
+        </button>
+      </div>
+    </div>
+  )
+}
